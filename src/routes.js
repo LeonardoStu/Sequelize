7d341539852c8bccdc79d97d30dee221ff1da562
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-07 14:01:51 -0300

    Termino da api

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const PlanetController = require('../controller/PlanetController')
 
 router.post('/planets', PlanetController.store)
+router.get('/planets', PlanetController.index)
+router.put('/planets/:id', PlanetController.put)
+router.delete('/planets/:id', PlanetController.delete)
 
 module.exports = router
